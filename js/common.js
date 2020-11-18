229a18bb2e32d621f34cbfd030d7a4ed2a55d9b5
dbsels12 <dbsels12@gmail.com>
2020-11-18 12:19:16 +0900

    mars

--- js/common.js
+++ js/common.js
@@ -1,11 +1,23 @@
 $(function(){
+  // 모바일용 전체메뉴
+  var menu = $('.menu_list > li > a')
+  menu.click(function(){
+    var subMenu = $(this).next('ul')
+    if(subMenu.is(':visible')){
+      subMenu.slideUp()
+    }else{
+      subMenu.slideDown()
+    }
+  })
+
+  
   //전체 메뉴
   
-   var hamberger = $('.hamberger')
+   var hamburger = $('.hamburger')
    var close = $('.menu .close')
    var menu = $('.menu')
  
-   hamberger.click(function(){
+   hamburger.click(function(){
      menu.show()
    })
  
